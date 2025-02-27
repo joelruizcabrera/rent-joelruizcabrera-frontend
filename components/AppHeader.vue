c9c1451022ab3843236aad442543c1633fd238aa
joelruizcabrera <schwegmann@hob-by-horse.de>
2025-02-27 14:40:49 +0100

    fix broken shopping cart badge

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex justify-between py-3 flex-row items-center w-screen px-4">
-    <div></div>
+  <div class="flex justify-center py-3 flex-row items-center w-screen px-4 relative">
     <NuxtLink to="/">
       <NuxtImg src="/img/logo.png" class="h-15 sm:h-20 sm:w-min w-auto"></NuxtImg>
     </NuxtLink>
-    <div class="text-white pe-3">
-      <NuxtLink to="/cart" class="relative">
-        <ShoppingBagIcon class="sm:size-8 size-6"></ShoppingBagIcon>
-        <span v-if="getProductLength > 0" class="animate__animated animate__fadeIn absolute top-0 end-0 inline-flex items-center sm:w-5 sm:h-5 w-4 h-4 justify-center rounded-full text-xs font-medium transform -translate-y-1/2 translate-x-1/2 bg-yellow-400 text-stone-950" v-html="getProductLength"></span>
+    <div class="text-white pe-3 absolute end-4">
+      <NuxtLink to="/cart">
+        <div class="relative">
+          <ShoppingBagIcon class="sm:size-8 size-6"></ShoppingBagIcon>
+          <span v-if="getProductLength > 0" class="animate__animated animate__fadeIn absolute top-0 end-0 inline-flex items-center sm:w-5 sm:h-5 w-4 h-4 justify-center rounded-full text-xs font-medium transform -translate-y-1/2 translate-x-1/2 bg-yellow-400 text-stone-950" v-html="getProductLength"></span>
+        </div>
       </NuxtLink>
     </div>
   </div>
