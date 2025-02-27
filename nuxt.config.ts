062597d19b523ce3b375a86eeb997eb90dbfd883
joelruizcabrera <schwegmann@hob-by-horse.de>
2025-02-27 13:13:27 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css', 'animate.css/animate.min.css'],
-
+  ssr: false,
   vite: {
     plugins: [
       tailwindcss(),
