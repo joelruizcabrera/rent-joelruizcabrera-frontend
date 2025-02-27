1dd3ec3fafe6a9ef1f064d4a061a3aacd2e16bf8
joelruizcabrera <schwegmann@hob-by-horse.de>
2025-02-27 13:06:14 +0100

    debug

--- stores/cart.ts
+++ stores/cart.ts
@@ -8,12 +8,14 @@ export const useCartStore = defineStore('cart', {
     actions: {
         addToCart(product: Object) {
             let tempArray = []
+            console.log(this.$state)
             console.log(this.$state.products)
+            console.log(state.products)
+            console.log(tempArray)
             if (this.$state.products.length >= 1) {
                 tempArray = this.$state.products.filter(x => x.id != product.id)
             }
             tempArray.push(product)
-            console.log(tempArray)
             this.$state.products = tempArray
             //localStorage.setItem('cart', JSON.stringify(this.products))
         },
